5a7f5f7d361147c224131302f7e05dd5e00709e5
Daniel Tan <danieltan.singapore@gmail.com>
2025-11-21 10:47:15 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module aoc-2020-go
+
+go 1.25.4
---
+++ utils/utils.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+
+}
+
+func ReadInts(path string) ([]int, error) {
+	lines, err := ReadLines(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var ints []int
+	for _, ss := range lines {
+		n, err := strconv.Atoi(ss)
+		if err != nil {
+			return nil, err
+		}
+		ints = append(ints, n)
+	}
+
+	return ints, nil
+
+}
